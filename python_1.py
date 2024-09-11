608627ec2da96e5edb33e89ae8c53a0f35d4caee
denyss <d.pnsw4444@gmail.com>
2024-09-11 10:34:33 +0300

    python_1.py

--- python_1.py
+++ python_1.py
@@ -1,9 +1,8 @@
 my_text = "Hello, World!"
 my_name = "Denis"
-my_last_name = "Panasiyk,"
+my_last_name = "Panasiyk"
 my_year = 16
 
-print(("Тип my_text:"), type(my_text), ("Тип my_name:"), type(
-    my_name), ("Тип my_last_text:"), type(my_last_name),
-    ("Тип my_year:"), type(my_year))
+type "my_text" type "my_name" type "my_last_name" type "my_year" 
+
 print(my_text, my_name, my_last_name, my_year)
